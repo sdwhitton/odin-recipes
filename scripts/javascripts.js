e18c17ad0759e4df2b895ce106425491f006b308
sdwhitton <stephen@retinaimages.co.uk>
2025-12-03 15:06:01 +0000

    added a function

--- scripts/javascripts.js
+++ scripts/javascripts.js
@@ -5,3 +5,13 @@ let lastName = "Doe";
 
 console.log(firstName);
 console.log(lastName);
+
+function favouriteAnimal(animal) {
+    return animal + " is my favourite animal!"
+}
+
+let message = favouriteAnimal("Kathy")
+console.log(message)
+
+let message = favouriteAnimal("Me!")
+console.log(message)
